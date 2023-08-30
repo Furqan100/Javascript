3ba0526ab58cd21476e8e689f75af4a30eb6de2a
Furqan100 <134839396+Furqan100@users.noreply.github.com>
2023-08-30 09:19:33 +0000

    variables config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+const customer_Id =190;
+let customer_Email="furqanlateef100@gmail.com"
+var customer_Name="Firqan";
+customer_Salary=10000;
+console.log([customer_Id,customer_Email,customer_Name,customer_Salary])
