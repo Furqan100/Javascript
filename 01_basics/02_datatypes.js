7067c623fbf2e24eaa8046fccb180b3ce417d880
Furqan100 <134839396+Furqan100@users.noreply.github.com>
2023-08-31 05:32:29 +0000

    datatypes comp[lete

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict" ;//treats all javascript code as newer version
+let user ="Furqan";
+let userisLogged = true;
+let age = 20;
+let value=null;
+let height;
+//symbol => is mostly used to find unqueness;
+console.log(typeof value);
+console.log(typeof height);
+console.log(typeof user);
+console.log(typeof age);
+
